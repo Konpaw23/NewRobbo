e1567b8a24b32a14788c37d9180838fcf5a941be
Canviarz <konrad.pawlowski23@gmail.com>
2025-06-12 16:04:13 +0200

    repaired idee

--- RobboMillennium/cmake-build-debug/cmake_install.cmake
+++ RobboMillennium/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Konrad/CLionProjects/RobboMillennium
+# Install script for directory: C:/Users/Konrad/CLionProjects/Robbo-Millennium/RobboMillennium
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Konrad/CLionProjects/RobboMillennium/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Konrad/CLionProjects/Robbo-Millennium/RobboMillennium/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
