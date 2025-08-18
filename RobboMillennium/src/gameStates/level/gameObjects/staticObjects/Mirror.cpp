a3fba5a0508179ca66dd096ab0737e970f80a0c4
Canviarz <konrad.pawlowski23@gmail.com>
2025-08-18 19:18:24 +0200

    kawalek funkcji luster

--- RobboMillennium/src/gameStates/level/gameObjects/staticObjects/Mirror.cpp
+++ RobboMillennium/src/gameStates/level/gameObjects/staticObjects/Mirror.cpp
@@ -22,4 +22,34 @@ bool Mirror::Exit(Robbo *robbo, Direction out)
     //TODO check if there is empty field next to mirror (start with Direction out, next right, left, and in other side)
     //TODO through robbo on empty field and return true
     //TODO return false when no empty field
+    Direction possibleFields[4] = {};
+    switch(out)
+    {
+        case LEFT:
+            possibleFields[0] = LEFT;
+            possibleFields[1] = UP;
+            possibleFields[2] = DOWN;
+            possibleFields[3] = RIGHT;
+            break;
+        case RIGHT:
+            possibleFields[0] = RIGHT;
+            possibleFields[1] = DOWN;
+            possibleFields[2] = UP;
+            possibleFields[3] = LEFT;
+            break;
+        case UP:
+            possibleFields[0] = UP;
+            possibleFields[1] = RIGHT;
+            possibleFields[2] = LEFT;
+            possibleFields[3] = DOWN;
+            break;
+        case DOWN:
+            possibleFields[0] = DOWN;
+            possibleFields[1] = LEFT;
+            possibleFields[2] = RIGHT;
+            possibleFields[3] = UP;
+            break;
+    }
+    if(this->level->GetObjectFromPosition(this->position.GetNext(out)) == nullptr)
+    
 }
