d1813185bf25bc75042565480812eddfbcb83732
Canviarz <konrad.pawlowski23@gmail.com>
2025-09-09 17:14:29 +0200

    class KillingObject that kills robbo when his on field next to

--- RobboMillennium/src/gameStates/level/gameObjects/KillingObject.cpp
+++ RobboMillennium/src/gameStates/level/gameObjects/KillingObject.cpp
@@ -1,6 +1,19 @@
 #include "../../../../include/gameStates/level/gameObjects/KillingObject.h"
+#include "../../../../include/gameStates/level/Level.h"
 
 KillingObject::KillingObject()
 {
     ;
+}
+
+void KillingObject::SearchAndKill()
+{
+    std::vector<Coordinates> fields = this->level->GetFieldsNextTo(this->position);
+    for(int i = 0; i < fields.size(); i++)
+    {
+        if(this->level->GetObjectNameFromPosition(fields[i]) == ROBBO)
+        {
+            this->level->GetObjectFromPosition(fields[i])->Destroy();
+        }
+    }
 }
